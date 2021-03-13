09c4033b8bf1733262612ebf4dbd3fccc7a4447b
Pistachio102 <tasnim282@gmail.com>
2021-03-13 23:30:14 +0600

    Complete: Key Scheduling, Encrypted, Decrypted

--- 1605102.py
+++ 1605102.py
@@ -11,22 +11,17 @@ count = 0
 bv1 = 00
 
 
-
 def convert_to_list(string):
     list1 = []
     list1[:0] = string
     return list1
 
 
-
-
 def convert_to_hex(list1):
     hex_list = []
-    for i in list1:
-        hex_list.append(hex(ord(i)))
+    for j in list1:
+        hex_list.append(hex(ord(j)))
     return hex_list
-
-
 
 
 def XOR(l1, l2):
@@ -34,23 +29,19 @@ def XOR(l1, l2):
     lenth = len(l1)
     c = 0
     while c < lenth:
-        #print(int(l1[c], 16))
-        #print(int(l2[c], 16))
+        # print(int(l1[c], 16))
+        # print(int(l2[c], 16))
         element = hex(int(l1[c], 16) ^ int(l2[c], 16))[2:4]
         new_list.append(element)
-        #print("element",element)
+        # print("element",element)
         c += 1
     return new_list
 
 
-
-
 def XOR_two_hex_values(hex_val1, hex_val2):
-    #print(type(hex_val1), type(hex_val2))
+    # print(type(hex_val1), type(hex_val2))
     added_hex_value = hex(int(hex_val1, 16) ^ int(hex_val2, 16))[2:4]
     return added_hex_value
-
-
 
 
 def create_matrix(list1):
@@ -61,8 +52,6 @@ def create_matrix(list1):
     matrix = numpy.array([a1, a2, a3, a4])
     # print(matrix)
     return matrix
-
-
 
 
 def add_two_matrix(matrix_1, matrix_2):
@@ -77,16 +66,12 @@ def add_two_matrix(matrix_1, matrix_2):
     return new_matrix
 
 
-
-
 def multiply_two_hex_values(hex_1, hex_2):
-    #hex_1 = BitVector(hexstring=hex_1)
-    #hex_2 = BitVector(hexstring=hex_2)
+    # hex_1 = BitVector(hexstring=hex_1)
+    # hex_2 = BitVector(hexstring=hex_2)
     s = hex_1.gf_multiply_modular(hex_2, bitvector_demo.AES_modulus, 8)
-    #print(hex(s.intValue()))
-    return  hex(s.intValue())
-
-
+    # print(hex(s.intValue()))
+    return hex(s.intValue())
 
 
 def substitute_bytes_of_matrix(m1):
@@ -99,10 +84,8 @@ def substitute_bytes_of_matrix(m1):
             s = bitvector_demo.Sbox[int_val]
             s = BitVector(intVal=s, size=8)
             m1[i][j] = s.get_bitvector_in_hex()
-            #print(m1[i][j])
+            # print(m1[i][j])
     return m1
-
-
 
 
 def inverse_substitute_bytes_of_matrix(m1):
@@ -115,10 +98,8 @@ def inverse_substitute_bytes_of_matrix(m1):
             s = bitvector_demo.InvSbox[int_val]
             s = BitVector(intVal=s, size=8)
             m1[i][j] = s.get_bitvector_in_hex()
-            #print(m1[i][j])
+            # print(m1[i][j])
     return m1
-
-
 
 
 def shift_rows_cyclically_by_offset(m1):
@@ -126,10 +107,8 @@ def shift_rows_cyclically_by_offset(m1):
         queue = deque(m1[i])
         queue.rotate(-i)
         m1[i] = queue
-    #print(m1)
+    # print(m1)
     return m1
-
-
 
 
 def inverse_shift_rows_cyclically_by_offset(m1):
@@ -137,17 +116,15 @@ def inverse_shift_rows_cyclically_by_offset(m1):
         queue = deque(m1[i])
         queue.rotate(i)
         m1[i] = queue
-    #print(m1)
+    # print(m1)
     return m1
-
-
 
 
 def mix_columns(m1):
     result = ([['0', '0', '0', '0'],
-              ['0', '0', '0', '0'],
-              ['0', '0', '0', '0'],
-              ['0', '0', '0', '0']])
+               ['0', '0', '0', '0'],
+               ['0', '0', '0', '0'],
+               ['0', '0', '0', '0']])
     # iterating by row of A
     for i in range(4):
 
@@ -156,18 +133,20 @@ def mix_columns(m1):
 
             # iterating by rows of B
             for k in range(4):
-                #result[i][j] += const_matrix[i][k] * m1[k][j]
-                result[i][j] = XOR_two_hex_values(result[i][j], multiply_two_hex_values(bitvector_demo.Mixer[i][k], BitVector(hexstring=m1[k][j])))
+                # result[i][j] += const_matrix[i][k] * m1[k][j]
+                result[i][j] = XOR_two_hex_values(result[i][j], multiply_two_hex_values(bitvector_demo.Mixer[i][k],
+                                                                                        BitVector(hexstring=m1[k][j])))
 
-    #print(result)
+    # print(result)
     result = numpy.array([result[0], result[1], result[2], result[3]])
     return result
 
+
 def inverse_mix_columns(m1):
     result = ([['0', '0', '0', '0'],
-              ['0', '0', '0', '0'],
-              ['0', '0', '0', '0'],
-              ['0', '0', '0', '0']])
+               ['0', '0', '0', '0'],
+               ['0', '0', '0', '0'],
+               ['0', '0', '0', '0']])
     # iterating by row of A
     for i in range(4):
 
@@ -176,10 +155,11 @@ def inverse_mix_columns(m1):
 
             # iterating by rows of B
             for k in range(4):
-                #result[i][j] += const_matrix[i][k] * m1[k][j]
-                result[i][j] = XOR_two_hex_values(result[i][j], multiply_two_hex_values(bitvector_demo.InvMixer[i][k], BitVector(hexstring=m1[k][j])))
+                # result[i][j] += const_matrix[i][k] * m1[k][j]
+                result[i][j] = XOR_two_hex_values(result[i][j], multiply_two_hex_values(bitvector_demo.InvMixer[i][k],
+                                                                                        BitVector(hexstring=m1[k][j])))
 
-    #print(result)
+    # print(result)
     result = numpy.array([result[0], result[1], result[2], result[3]])
     return result
 
@@ -219,20 +199,16 @@ def g(list1):
     rc = []
     # print(hex(bv3.intValue()))
     rc.append(hex(bv3.intValue()))
-    #print(rc[0])
+    # print(rc[0])
     added = XOR(sbyte, rc)
     g_list[0] = added[0]
-    #print('g list : ',g_list[0])
+    # print('g list : ',g_list[0])
     return g_list
-
-
 
 
 def concatenate_the_lists(a1, a2, a3, a4):
     list1 = a1 + a2 + a3 + a4
     return list1
-
-
 
 
 def generate_round_keys(key_list):
@@ -263,10 +239,7 @@ def generate_round_keys(key_list):
     return all_round_keys
 
 
-
-
 def schedule_key(key):
-
     length_of_key = len(key)
 
     if length_of_key < 16:
@@ -285,14 +258,12 @@ def schedule_key(key):
         hex_list[i] = str(hex_list[i])[2: 4:]
         i += 1
     print()
-    print("Key In ASCII: ",key)
+    print("Key In ASCII: ", key)
     print("Key In Hex: ", end='')
     for i in range(len(hex_list)):
         print(hex_list[i], end='')
     print()
     return generate_round_keys(hex_list)
-
-
 
 
 def encrypt(plaintext, keys):
@@ -302,27 +273,24 @@ def encrypt(plaintext, keys):
         hex_list_of_plaintext[i] = str(hex_list_of_plaintext[i])[2: 4:]
         i += 1
     # print(hex_of_plaintext)
-    #Adding round key
+    # Adding round key
     matrix = (add_two_matrix(create_matrix(hex_list_of_plaintext), create_matrix(keys[0]))).transpose()
     # print(matrix)
-    #Loop for 10 rounds
+    # Loop for 10 rounds
     round_number = 1
     while round_number < 11:
         matrix = substitute_bytes_of_matrix(matrix)
         matrix = shift_rows_cyclically_by_offset(matrix)
         if round_number != 10:
             matrix = mix_columns(matrix)
-        #print(round_number)
+        # print(round_number)
         matrix = add_two_matrix(matrix, create_matrix(keys[round_number]).transpose())
         round_number += 1
 
-    #print(matrix)
-    #print(matrix[0], matrix[1], matrix[2], matrix[3])
-
+    # print(matrix)
+    # print(matrix[0], matrix[1], matrix[2], matrix[3])
 
     return matrix
-
-
 
 
 def decrypt(matrix, keys):
@@ -335,27 +303,23 @@ def decrypt(matrix, keys):
         matrix = add_two_matrix(matrix, create_matrix(keys[round_number]).transpose())
         if round_number != 0:
             matrix = inverse_mix_columns(matrix)
-        #print(round_number)
+        # print(round_number)
         round_number -= 1
 
     return matrix
 
 
-
 def chunked(size, source):
     for i in range(0, len(source), size):
-        yield source[i:i+size]
-
-
+        yield source[i:i + size]
 
 
 if __name__ == '__main__':
 
-
     key = input('Key: ')
     plaintext = input('Plaintext: ')
 
-    # ----------KEY SCHEDULING--------------#
+    #--------------KEY SCHEDULING--------------#
 
     start_of_key_scheduling = time.time()
     all_round_keys = schedule_key(key)
@@ -363,21 +327,19 @@ if __name__ == '__main__':
 
     print()
 
-
-    print('Plaintext In ASCII: ', plaintext)        #For printing in the console
-    print('Plaintext In Hex: ', end='')             #For printing in the console
-    plaintext_in_hex = convert_to_hex(plaintext)    #For printing in the console
-    for i in range(len(plaintext_in_hex)):          #For printing in the console
-        print((plaintext_in_hex[i])[2:4], end='')   #For printing in the console
-
-
+    print('Plaintext In ASCII: ', plaintext)  # For printing in the console
+    print('Plaintext In Hex: ', end='')  # For printing in the console
+    plaintext_in_hex = convert_to_hex(plaintext)  # For printing in the console
+    for i in range(len(plaintext_in_hex)):  # For printing in the console
+        print((plaintext_in_hex[i])[2:4], end='')  # For printing in the console
 
     print()
     print()
 
-    # ----------ENCRYPTION--------------#
+    #--------------ENCRYPTION--------------#
 
-    plaintext_in_list = list(chunked(16, plaintext)) #Processing plaintext into 16 bytes of chunks and adding them to the list
+    plaintext_in_list = list(
+        chunked(16, plaintext))  # Processing plaintext into 16 bytes of chunks and adding them to the list
     encrypted_matrix_list = []
 
     start_of_encryption = time.time()
@@ -392,14 +354,13 @@ if __name__ == '__main__':
         encrypted_matrix_list.append(encrypted_matrix)
     end_of_encryption = time.time()
 
-
     print("Cipher Text In ASCII: ", end='')
     for matrix in encrypted_matrix_list:
         matrix = matrix.transpose()
         for row in range(len(matrix)):
             for column in range(len(matrix[row])):
                 hex_string = matrix[row][column]
-                ascii_string = chr(int(hex_string,16))
+                ascii_string = chr(int(hex_string, 16))
                 print(ascii_string, end='')
 
     print()
@@ -411,13 +372,10 @@ if __name__ == '__main__':
             for column in range(len(matrix[row])):
                 print(matrix[row][column], end='')
 
-
-
-
     print()
     print()
 
-    #----------DECRYPTION--------------#
+    #--------------DECRYPTION--------------#
 
     decrypted_matrix_list = []
 
@@ -427,14 +385,13 @@ if __name__ == '__main__':
         decrypted_matrix_list.append(decrypted_matrix)
     end_of_decryption = time.time()
 
-
     print("Deciphered Text In ASCII: ", end='')
     for matrix in decrypted_matrix_list:
         matrix = matrix.transpose()
         for row in range(len(matrix)):
             for column in range(len(matrix[row])):
                 hex_string = matrix[row][column]
-                ascii_string = chr(int(hex_string,16))
+                ascii_string = chr(int(hex_string, 16))
                 print(ascii_string, end='')
 
     print()
@@ -447,11 +404,10 @@ if __name__ == '__main__':
             for column in range(len(matrix[row])):
                 print(matrix[row][column], end='')
 
-
     print()
     print()
 
-    # ----------EXECUTION TIME--------------#
+    #--------------EXECUTION TIME--------------#
 
     print("Execution Time: ")
     print("Key Sceduling: ", end_of_key_scheduling - start_of_key_scheduling)
